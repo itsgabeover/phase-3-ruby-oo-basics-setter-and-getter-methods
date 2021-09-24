d2f3ef108bdfdd8eb6baad6e75537e68378ee406
Gabriel Miranda <gabe.enrique.miranda@gmail.com>
2021-09-23 19:48:57 -0700

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
 
+    def name
+        @name
+    end
+
+    def breed=(arg)
+        @breed = arg 
+    end
+
+    def breed
+        @breed
+    end
 end
